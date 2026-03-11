5d8348d9f89ab519f2c1a622c9e5e3d53f977599
May <ferrand.mayeul@gmail.com>
2026-03-11 11:58:22 +0100

    Cour Animation

---
+++ Assets/CourAnimation/Script/AnimatorDriver.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+
+public class AnimatorDriver : MonoBehaviour
+{
+
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private async void Start()
+    {
+        await Awaitable.WaitForSecondsAsync(3f);
+
+        _animator.SetTrigger("Switch");
+    }
+
+}
