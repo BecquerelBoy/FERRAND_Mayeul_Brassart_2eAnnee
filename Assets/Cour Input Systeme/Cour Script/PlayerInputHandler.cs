818a4177b4327c0fb85bac02e72f43ad8c5c6b70
May <ferrand.mayeul@gmail.com>
2026-03-30 16:27:21 +0200

    Cour input system

---
+++ Assets/Cour Input Systeme/Cour Script/PlayerInputHandler.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputHandler : MonoBehaviour
+{
+    private InputSystem_Gun _inputSystemGun;
+
+    private void Start()
+    {
+        _inputSystemGun = new InputSystem_Gun();
+        _inputSystemGun.Player.Shoot.performed += Shoot();
+    }
+
+    private void Update()
+    {
+        if (Keyboard.current.zKey.wasPressedThisFrame)
+        {
+            Debug.Log("Go forward");
+        }
+    }
+
+    public void Shoot(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Shoot");
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log($"Move input value : {context.ReadValue<Vector2>()}");
+        }
+    }
+}
